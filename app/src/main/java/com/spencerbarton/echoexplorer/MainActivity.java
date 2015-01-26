cac9821b88cbdffac00f79b23f4ffd7126ee0e5e
Brandon Perez <bmperez@andrew.cmu.edu>
2015-01-25 21:23:27 -0500

    Added in some infrastructure for playing sounds on a button push. Currently not working.

--- app/src/main/java/com/spencerbarton/echoexplorer/MainActivity.java
+++ app/src/main/java/com/spencerbarton/echoexplorer/MainActivity.java
@@ -1,19 +1,55 @@
 package com.spencerbarton.echoexplorer;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.media.AudioManager;
+import android.os.IBinder;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
+    private PlaySound mService;
+    private boolean mBound = false;
+    public static String EXTRA_AUDIO = "com.spencerbarton.echoexplorer.AUDIO";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Initialize
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setup audio to be controlled by the user
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        Log.d("logging", "onCreate called");
     }
 
+    @Override
+    protected void onStart() {
+        Log.d("logging", "onStart called");
+        super.onStart();
+
+        // Bind audio service
+        Intent intent = new Intent(this, PlaySound.class);
+        intent.putExtra(EXTRA_AUDIO, R.raw.song);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +72,28 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void playAudio(View view) {
+        Log.d("logging", "Playing audio");
+        if (mBound) {
+            Log.d("logging", "Device is bound");
+            mService.playAudio();
+        }
+    }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            PlaySound.PlayAudioBinder binder = (PlaySound.PlayAudioBinder) service;
+            mService = binder.getService();
+            mBound = true;
+            Log.d("logging", "Service Connected");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
+        }
+    };
+
 }
---
+++ app/src/main/java/com/spencerbarton/echoexplorer/PlaySound.java
@@ -0,0 +1,69 @@
+package com.spencerbarton.echoexplorer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.io.IOException;
+
+public class PlaySound extends Service {
+    private final static String TAG = "PlaySoundService";
+    private final IBinder mBinder = new PlayAudioBinder();
+    private MediaPlayer mMediaPlayer;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.e(TAG,"Binding service");
+        initAudio(intent);
+        return mBinder;
+    }
+
+    @Override
+    public void onDestroy() {
+        stopAudio();
+    }
+
+    public void playAudio() {
+        try {
+            Log.d(TAG, "Preparing Audio");
+            mMediaPlayer.prepare();
+            Log.d(TAG, "Starting Audio");
+            mMediaPlayer.start();
+        } catch (IllegalStateException | IOException e) {
+            Log.e(TAG, "7777:" + e.getMessage());
+        }
+    }
+
+    public void stopAudio() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.stop();
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+    }
+
+    public class PlayAudioBinder extends Binder {
+
+        PlaySound getService() { return PlaySound.this; }
+
+    }
+
+    private void initAudio(Intent intent) {
+        int audioFile = intent.getIntExtra(MainActivity.EXTRA_AUDIO, 0);
+        mMediaPlayer = MediaPlayer.create(this, audioFile);
+        Log.d(TAG, "Created Media player, got audio file");
+        Log.d(TAG, "audioFile:"+String.valueOf(audioFile));
+
+        // Register termination
+        mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                stopAudio();
+            }
+        });
+    }
+
+}
