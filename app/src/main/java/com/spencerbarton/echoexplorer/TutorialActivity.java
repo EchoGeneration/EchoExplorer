d3fb011a3659a616f7cd78e6ce164c544af29612
Spencer Barton <sbarton272@gmail.com>
2015-02-18 14:28:29 -0500

    Added eval activity

--- app/src/main/java/com/spencerbarton/echoexplorer/TutorialActivity.java
+++ app/src/main/java/com/spencerbarton/echoexplorer/TutorialActivity.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-
 public class TutorialActivity extends ActionBarActivity {
 
     private final static String TAG = "TutorialActivity";
