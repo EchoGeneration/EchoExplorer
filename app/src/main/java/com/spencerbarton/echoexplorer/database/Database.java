ee64872ec1f27fe9b0e4a75509b4ae44750aeb46
Brandon Perez <bmperez@andrew.cmu.edu>
2015-05-23 12:41:17 -0400

    Finshed up javadoc documentation for the Database class.

--- app/src/main/java/com/spencerbarton/echoexplorer/database/Database.java
+++ app/src/main/java/com/spencerbarton/echoexplorer/database/Database.java
@@ -72,7 +72,8 @@ public abstract class Database<T> {
      *                     This is used to access the assets folder and the local storage.
      * @param staticDatabase Indicates whether or not this database is static.
      * @throws SQLiteException The database file is not properly formatted.
-     * @throws IOException The database file is not writeable or readable.
+     * @throws IOException The database file is not writeable or readable, or the dbName does
+     *                     does not exist in the assets folder.
      **/
     public Database(Context context, String databaseName, boolean staticDatabase) throws
             SQLiteException, IOException
@@ -116,7 +117,7 @@ public abstract class Database<T> {
      *             with a corresponding '?' in query.
      * @param cls The class object of the type T. Used to create an generic array of type T.
      * @return The results of the query in an array, or null if the query returns no results.
-     */
+     **/
     public T[] unbufferedQuery(String query, String[] args, Class<T> cls)
     {
         Cursor result = bufferedQuery(query, args);
@@ -133,23 +134,23 @@ public abstract class Database<T> {
      * name. Given a cursor object, and a column name, returns the value stored in that column of
      * the current row pointed to by the cursor.
      *
-     * @param dbCursor The cursor object with the results of a query.
+     * @param dbCursor The Cursor object with the results of a query.
      * @param colName The name of the column to access.
      * @return The value of the given column.
-     */
-    // Provides a convenient wrapper to access a column
+     **/
     public static String getColumnByName(Cursor dbCursor, String colName) {
         return dbCursor.getString(dbCursor.getColumnIndex(colName));
     }
 
     /**
      * Given a Cursor object, retrieves all of the entries in the Cursor object and stores them
-     * into a generic array of type T.
+     * into a generic array of type T. This function retrieves all of the entries in the Cursor
+     * object, regardless of its current position (the function moves to the first entry).
      *
      * @param cursor The Cursor object to retrieve the entries from.
      * @param cls The class object of the type T. Used to create an generic array of type T.
      * @return All of the entries in the Cursor object collected into the array.
-     */
+     **/
     public T[] getAllEntries(Cursor cursor, Class<T> cls)
     {
         T[] results = (T[]) Array.newInstance(cls, cursor.getCount());
@@ -168,32 +169,55 @@ public abstract class Database<T> {
         return results;
     }
 
+    /**
+     * The abstract method which takes a Cursor object from the result of query, and packs the
+     * columns from a query result into the generic type T, which represents the schema of the
+     * table. The subclasses must implement this method.
+     *
+     * @param cursor The Cursor object to pack an entry into type T.
+     * @return The next entry in the cursor, with the columns of the result packed into the type T.
+     **/
     public abstract T packCursorEntry(Cursor cursor);
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     // Private Methods
     ////////////////////////////////////////////////////////////////////////////////////////////////
 
+    /**
+     * Opens up the static, readonly database corresponding to dbName. If the database does not
+     * exist on disk or the application has been updated, then it is retrieved from the assets
+     * folder, copied to the database directory, and opened. Otherwise, the database is simply
+     * opened.
+     *
+     * @param context The application's context. Used to access the assets folder of the
+     *                application.
+     * @param dbName The name of the database to open.
+     * @return A handle to the SQLiteDatabase object representing the connection to the database.
+     * @throws SQLiteException The database file is not properly formatted.
+     * @throws IOException The database file is not writeable or readable, or the dbName does
+     *                     does not exist in the assets folder.
+     **/
     private SQLiteDatabase openStaticDatabase(Context context, String dbName) throws
             SQLiteException, IOException
     {
         Log.i(TAG, "Opening up database: " + dbName);
         String dbPath = DB_DIR + dbName;
 
-        /* If the database does not exist, then copy it in from the assets folder. Make sure that
-         * the parent directory exists, and create it if it does not exist. Otherwise, if the
-         * application has been updated, then copy the new database in from the new package and
-         * update the file on disk that tracks the current version.
+        /* If the database does not exist or the application has been updated, then copy it in from
+         * the assets folder of the application. Then, update the version file on disk, which tracks
+         * the version of application this database corresponds to.
          */
         File dbFile = new File(dbPath);
         if (!dbFile.exists()) {
             Log.i(TAG, "Database does not exist. Copying database in from the assets folder");
 
+            // Create the database directory if it does not exist
             File dbDirectory = new File(DB_DIR);
             if (!dbDirectory.exists()) {
                 dbDirectory.mkdirs();
             }
             copyDbFromAssets(context, dbName, dbPath);
+            writeVersionFile(mVersionPath);
         } else if (applicationUpdated(mVersionPath)) {
             copyDbFromAssets(context, dbName, dbPath);
             writeVersionFile(mVersionPath);
@@ -203,6 +227,13 @@ public abstract class Database<T> {
         return SQLiteDatabase.openDatabase(dbPath, mCursorFactory, SQLiteDatabase.OPEN_READONLY);
     }
 
+    /**
+     * Opens up a dynamic, writeable database corresponding to dbName. If the database does not
+     * exist on disk, then it is created.
+     *
+     * @param dbName The name of the database to open.
+     * @return A handle to the SQLiteDatabase object representing the connection to the database.
+     **/
     private SQLiteDatabase openDatabase(String dbName) {
         Log.i(TAG, "Opening up database: " + dbName);
         String dbPath = DB_DIR + dbName;
@@ -210,30 +241,67 @@ public abstract class Database<T> {
         return SQLiteDatabase.openDatabase(dbPath, mCursorFactory, SQLiteDatabase.OPEN_READWRITE);
     }
 
+    /**
+     * Copies the database corresponding to dbName from the assets folder to the destination path
+     * destPath. If the destPath already exists, then it is overwritten with the file in the assets
+     * folder of the application.
+     *
+     * @param context The application's context. Used to access the assets folder of the
+     *                application.
+     * @param dbName The name of the database to copy from the assets folder.
+     * @param destPath The place to copy the database to.
+     * @throws IOException The destination file is not writeable or readable, or the dbName does
+     *                     does not exist in the assets folder.
+     **/
     private static void copyDbFromAssets(Context context, String dbName, String destPath) throws
             IOException
     {
+        // Open the database in assets and the destination path
         InputStream source = openDbFromAssets(context, dbName);
         OutputStream dest = new FileOutputStream(destPath);
 
         final int bufSize = 1024;
         byte[] buffer = new byte[bufSize];
 
+        // Copy the database to the destination one segment at a time.
         int bytesRead = source.read(buffer);
         while (bytesRead != -1) {
             dest.write(buffer, 0, bufSize);
             bytesRead = source.read(buffer);
         }
 
+        // Flush the destination file, and close both files
         dest.flush();
         dest.close();
         source.close();
     }
 
+
+    /**
+     * Opens the database in the assets folder corresponding to dbName.
+     *
+     * @param context The application's context. Used to access the assets folder of the
+     *                application.
+     * @param dbName The name of the database to open from the assets folder.
+     * @return A handle to the open file corresponding to the dbName file in the assests folder.
+     * @throws IOException The file dbName does not exist in the assets folder.
+     */
     private static InputStream openDbFromAssets(Context context, String dbName) throws IOException {
         return context.getAssets().open(dbName);
     }
 
+    /**
+     * Checks if the application has been updated since the application was last run. The
+     * version is kept track of on an on-disk file that simply holds the version code of the last
+     * time the application was run. This is necessary because when the application is updated (or
+     * reloaded from Android Studio), the database may have been updated. Without the version file,
+     * we would not know when to update the database with a new one from the assets folder.
+     *
+     * @param versionPath The path to the version file, which tracks the version of the application
+     *                    the last time it was run.
+     * @return True if the application has been updated (or is new), false otherwise.
+     * @throws FileNotFoundException This exception will never be thrown.
+     */
     private static boolean applicationUpdated(String versionPath) throws FileNotFoundException
     {
         File versionFile = new File(versionPath);
@@ -253,9 +321,15 @@ public abstract class Database<T> {
         return (oldVersion != curVersion);
     }
 
+    /**
+     * Updates the version file on disk with the current version of the application.
+     *
+     * @param versionPath The path to the version file.
+     * @throws IOException The version file is not readable or writeable.
+     */
     private static void writeVersionFile(String versionPath) throws IOException
     {
-        // Attempt to get the current version of the application
+        // Get the current application version.
         int curVersion = BuildConfig.VERSION_CODE;
 
         // Write the new version out to the file
